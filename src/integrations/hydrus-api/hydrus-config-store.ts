12a553be6bfa482d6c20e30e65c66f2983fdcf96
Hyaway Dev <dev@hyaway.com>
2025-11-09 19:41:09 -0800

    zustant client

--- src/integrations/hydrus-api/hydrus-config-store.ts
+++ src/integrations/hydrus-api/hydrus-config-store.ts
@@ -1,10 +1,13 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { HydrusApiClient } from "./api-client";
 import type { StateCreator } from "zustand";
+import { getContext } from "@/integrations/tanstack-query/root-provider.tsx";
 
 type AuthState = {
   api_access_key: string;
   api_endpoint: string;
+  apiClient: HydrusApiClient | null;
   actions: {
     setApiCredentials: (accessKey: string, endpoint: string) => void;
     reset: () => void;
@@ -14,11 +17,20 @@ type AuthState = {
 const authSlice: StateCreator<AuthState> = (set, _get, store) => ({
   api_access_key: "",
   api_endpoint: "",
+  apiClient: null,
   actions: {
-    setApiCredentials: (accessKey: string, endpoint: string) =>
-      set({ api_access_key: accessKey, api_endpoint: endpoint }),
+    setApiCredentials: (accessKey: string, endpoint: string) => {
+      const apiClient = new HydrusApiClient(endpoint, accessKey);
+      set({ api_access_key: accessKey, api_endpoint: endpoint, apiClient });
+      getContext().queryClient.clear();
+    },
     reset: () => {
-      set(store.getInitialState());
+      const initialState = store.getInitialState();
+      set({
+        ...initialState,
+        apiClient: null,
+      });
+      getContext().queryClient.clear();
     },
   },
 });
@@ -31,12 +43,18 @@ export const useAuthStore = create<AuthState>()(
       api_access_key: state.api_access_key,
       api_endpoint: state.api_endpoint,
     }),
+    onRehydrateStorage: () => (state) => {
+      if (state) {
+        state.actions.setApiCredentials(
+          state.api_access_key,
+          state.api_endpoint,
+        );
+      }
+    },
   }),
 );
 
-export const useApiAccessKey = () =>
-  useAuthStore((state) => state.api_access_key);
-
-export const useApiEndpoint = () => useAuthStore((state) => state.api_endpoint);
+export const useHydrusApiClient = () =>
+  useAuthStore((state) => state.apiClient);
 
 export const useAuthActions = () => useAuthStore((state) => state.actions);
