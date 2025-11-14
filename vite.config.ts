7d3f7003433aef718fda2f2fa5e972831507f161
Hyaway Dev <dev@hyaway.com>
2025-11-14 00:16:23 -0800

    movee tailwind up

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
@@ -15,7 +16,6 @@ export default defineConfig({
       semicolons: true,
     }),
     viteReact(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
