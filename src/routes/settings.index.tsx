5cab95b9ceb5d48a45e67236919d87589628ee38
Hyaway Dev <dev@hyaway.com>
2025-12-14 00:21:31 -0800

    api settings redirect

--- src/routes/settings.index.tsx
+++ src/routes/settings.index.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/settings/")({
   component: RouteComponent,
   beforeLoad: () => {
-    throw redirect({ to: "/settings/account" });
+    throw redirect({ to: "/settings/client-api" });
   },
 });
 
