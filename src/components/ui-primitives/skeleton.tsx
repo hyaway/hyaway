5fb93f979c85dbfb0643f64e4b80062f8288607f
Hyaway Dev <dev@hyaway.com>
2025-12-24 03:54:38 -0800

    skeleton delay to not flash

--- src/components/ui-primitives/skeleton.tsx
+++ src/components/ui-primitives/skeleton.tsx
@@ -4,7 +4,10 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-muted animate-pulse rounded-xl", className)}
+      className={cn(
+        "bg-muted animate-pulse rounded-xl opacity-0 delay-100",
+        className,
+      )}
       {...props}
     />
   );
