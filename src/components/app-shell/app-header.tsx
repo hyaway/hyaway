1fe776bea6f55e0a203df44de5c02f0bb63e5d2a
Hyaway Dev <dev@hyaway.com>
2026-01-02 00:33:56 -0800

    right sidebar opener

--- src/components/app-shell/app-header.tsx
+++ src/components/app-shell/app-header.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { AppBreadcrumb } from "@/components/app-shell/app-breadcrumb";
+import { useRightSidebarHasContent } from "@/components/app-shell/right-sidebar-portal";
 import { Separator } from "@/components/ui-primitives/separator";
 import { SidebarTrigger } from "@/components/ui-primitives/sidebar";
 
 export function AppHeader() {
+  const hasRightSidebar = useRightSidebarHasContent();
+
   return (
     <div className="short:h-(--header-height-short) flex h-(--header-height) shrink-0 items-center gap-2 px-4">
       <SidebarTrigger side="left" className="-ml-1" />
@@ -11,6 +16,15 @@ export function AppHeader() {
         className="my-auto mr-2 data-[orientation=vertical]:h-4"
       />
       <AppBreadcrumb />
+      {hasRightSidebar && (
+        <>
+          <Separator
+            orientation="vertical"
+            className="my-auto ml-auto data-[orientation=vertical]:h-4"
+          />
+          <SidebarTrigger side="right" className="-mr-1" />
+        </>
+      )}
     </div>
   );
 }
