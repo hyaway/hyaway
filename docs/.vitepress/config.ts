3fb065f4fea73950985bef2566e91b4d38e93e44
Hyaway Dev <dev@hyaway.com>
2026-01-18 00:44:52 -0800

    favicon for docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   title: "hyAway",
   description: "Documentation for hyAway",
   cleanUrls: true,
+  head: [
+    ["link", { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+  ],
   themeConfig: {
     nav: [{ text: "Setup", link: "/setup" }],
     sidebar: [
