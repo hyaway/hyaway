430674865d9c9c5e99890d4ab4dda497d4915e10
Hyaway Dev <dev@hyaway.com>
2026-05-10 18:26:46 -0700

    simplify file page

--- src/routes/_auth/(galleries)/search.$fileId.tsx
+++ src/routes/_auth/(galleries)/search.$fileId.tsx
@@ -2,27 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { createFileRoute } from "@tanstack/react-router";
-import { useMemo } from "react";
-import z from "zod";
 
-import type { HydrusTagSearch } from "@/integrations/hydrus-api/models";
-import { HydrusFileSortType } from "@/integrations/hydrus-api/models";
-import { useSearchFilesQuery } from "@/integrations/hydrus-api/queries/search";
+import { useCommittedSearchFilesQuery } from "./-components/use-committed-search-query";
 import { FileDetail } from "@/components/file-detail/file-detail";
 import { useFileContextNavigation } from "@/hooks/use-file-context-navigation";
-
-const SearchParamsSchema = z.object({
-  tags: z
-    .string()
-    .transform((val) => val.split(",").filter(Boolean))
-    .or(z.array(z.string()))
-    .optional(),
-  sortType: z.number().optional().catch(undefined),
-  sortAsc: z.boolean().optional().catch(undefined),
-});
+import { PRIMARY_SEARCH_KEY } from "@/stores/search-queries-store";
 
 export const Route = createFileRoute("/_auth/(galleries)/search/$fileId")({
-  validateSearch: (search) => SearchParamsSchema.parse(search),
   component: RouteComponent,
   beforeLoad: ({ params }) => ({
     getTitle: () => `File ${params.fileId}`,
@@ -31,28 +17,10 @@ export const Route = createFileRoute("/_auth/(galleries)/search/$fileId")({
 
 function RouteComponent() {
   const { fileId } = Route.useParams();
-  const { tags: selectedTags, sortType, sortAsc } = Route.useSearch();
   const fileIdNum = Number(fileId);
 
-  // Tags come directly from URL params (set by the index page's getFileLink)
-  const searchTags: HydrusTagSearch = useMemo(
-    () => selectedTags ?? [],
-    [selectedTags],
-  );
-
-  const searchOptions = useMemo(
-    () => ({
-      file_sort_type: (sortType ??
-        HydrusFileSortType.ImportTime) as HydrusFileSortType,
-      file_sort_asc: sortAsc ?? false,
-    }),
-    [sortType, sortAsc],
-  );
-
-  const { data, isLoading, isError } = useSearchFilesQuery(
-    searchTags,
-    searchOptions,
-  );
+  const { data, isLoading, isError } =
+    useCommittedSearchFilesQuery(PRIMARY_SEARCH_KEY);
 
   const fileIds = data?.file_ids;
 
