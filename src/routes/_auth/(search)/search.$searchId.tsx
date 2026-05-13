916242e80b1e78441b0c512dd73dc3fee20ab898
Hyaway Dev <dev@hyaway.com>
2026-05-12 23:13:30 -0700

    remove override

--- src/routes/_auth/(search)/search.$searchId.tsx
+++ src/routes/_auth/(search)/search.$searchId.tsx
@@ -2,17 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Outlet, createFileRoute } from "@tanstack/react-router";
-import {
-  commitSearch,
-  getSearchDisplayName,
-} from "@/stores/search-queries-store";
+import { getSearchDisplayName } from "@/stores/search-queries-store";
 
 export const Route = createFileRoute("/_auth/(search)/search/$searchId")({
   component: () => <Outlet />,
-  beforeLoad: ({ params }) => {
-    commitSearch(params.searchId);
-    return {
-      getTitle: () => getSearchDisplayName(params.searchId),
-    };
-  },
+  beforeLoad: ({ params }) => ({
+    getTitle: () => getSearchDisplayName(params.searchId),
+  }),
 });
