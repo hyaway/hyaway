964bbbded43a4c4662957b122e80f0597319ef37
Hyaway Dev <dev@hyaway.com>
2026-05-16 19:43:57 -0700

    remove settings

--- src/routes/_auth/(search)/-components/search-settings-popover.tsx
+++ src/routes/_auth/(search)/-components/search-settings-popover.tsx
@@ -1,23 +1,12 @@
 // Copyright 2026 hyAway contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useSearchPageState } from "../-hooks/use-search-page-state";
 import { ThumbnailGalleryDisplaySettingsContent } from "@/components/thumbnail-gallery/thumbnail-gallery-display-settings-popover";
-import { SwitchField } from "@/components/settings/setting-fields";
 import { SettingsPopover } from "@/components/settings/settings-popover";
 
 export function SearchSettingsPopover() {
-  const { instantSearch, setInstantSearch } = useSearchPageState();
-
   return (
     <SettingsPopover label="Settings">
-      <SwitchField
-        id="search-instant-search-switch"
-        label="Instant search"
-        description="Run this search as soon as its query or sort changes."
-        checked={instantSearch}
-        onCheckedChange={setInstantSearch}
-      />
       <ThumbnailGalleryDisplaySettingsContent />
     </SettingsPopover>
   );
